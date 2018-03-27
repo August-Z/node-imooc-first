49a48d24de0ad3e514324641b3f01cfebdbef08f
August-Z <402279076@qq.com>
2018-03-27 23:00:13 +0800

    启动我的第一个 node.js 服务

--- create-server.js
+++ create-server.js
@@ -2,11 +2,13 @@ var http = require('http');
 
 /**
  * 收到来自 3000 端口的请求后，执行其中的回调函数，
- * 函数分别为 req 请求体  res 相应体
+ * 函数分别为
+ * req 请求体，从哪来的，GET or POST ??? 等等
+ * res 响应体，服务器端返回的信息，比如我们返回一个 Hello world
  */
 http.createServer(function(req, res) {
   res.writeHead(200, {'Content-type': 'text/plain'});
-  res.end('Hello world\n');
+  res.end('Hello Node.js\n');
 }).listen(3000,'0.0.0.0');
 
 console.log('Server running at http://127.0.0.1:3000/')
