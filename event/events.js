40efa8d6b2e7fa555b41b0878fc847349c720cf0
August-Z <402279076@qq.com>
2018-03-30 13:16:04 +0800

    eventEmitter 注册监听器、事件

--- event/events.js
+++ event/events.js
@@ -5,6 +5,24 @@ eventEmitter.on('hello-world', (name) => {
   console.log('hello world! My name is ' + name)
 })
 
+eventEmitter.once('only-one', (num) => {
+  console.log('only-one ' + num)
+  clearInterval(timer)
+})
+
+eventEmitter.addListener('event loop', (val) => {
+  console.log(val)
+})
+
 setTimeout(() => {
   eventEmitter.emit('hello-world', 'August')
 })
+
+let i = 0
+let timer = setInterval(() => {
+  eventEmitter.emit('only-one', ++i)
+}, 500)
+
+setInterval(() => {
+  eventEmitter.emit('event loop', ++i)
+}, 500)
